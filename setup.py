1ad5d38d6841e07955c26412fc7352fa4eb5b48e
Toni Heittola <toni.heittola@gmail.com>
2017-06-09 11:58:24 +0300

    Increase version number

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ long_description = open('README.rst').read()
 
 setup(
     name='sed_eval',
-    version='0.1.5',
+    version='0.1.6',
     description='Evaluation toolbox for Sound Event Detection',
     author='Toni Heittola',
     author_email='toni.heittola@gmail.com',
