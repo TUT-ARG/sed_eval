7913c3eb2d87043b015eaaba2f72c952a863437e
toni_heittola <toni.heittola@gmail.com>
2017-07-22 13:15:16 +0300

    Add test for direct usage (without loading event lists from disk)

--- tests/test_sound_event.py
+++ tests/test_sound_event.py
@@ -265,3 +265,128 @@ def test_audioset():
     nose.tools.eq_(results['overall']['error_rate']['error_rate'], 0.0)
 
     nose.tools.eq_(results['overall']['f_measure']['f_measure'], 1.0)
+
+
+def test_direct_use_segment():
+    reference_event_list = sed_eval.util.EventList(
+        [
+            {
+                'event_label': 'car',
+                'event_onset': 0.0,
+                'event_offset': 2.5,
+                'file': 'audio/street/b099.wav',
+                'scene_label': 'street'
+            },
+            {
+                'event_label': 'car',
+                'event_onset': 2.8,
+                'event_offset': 4.5,
+                'file': 'audio/street/b099.wav',
+                'scene_label': 'street'
+            },
+            {
+                'event_label': 'car',
+                'event_onset': 6.0,
+                'event_offset': 10.0,
+                'file': 'audio/street/b099.wav',
+                'scene_label': 'street'
+            }
+        ]
+    )
+
+    estimated_event_list = sed_eval.util.EventList(
+        [
+            {
+                'event_label': 'car',
+                'event_onset': 1.0,
+                'event_offset': 3.5,
+                'file': 'audio/street/b099.wav',
+                'scene_label': 'street'
+            },
+            {
+                'event_label': 'car',
+                'event_onset': 7.0,
+                'event_offset': 8.0,
+                'file': 'audio/street/b099.wav',
+                'scene_label': 'street'
+            }
+        ]
+    )
+
+    segment_based_metrics = sed_eval.sound_event.SegmentBasedMetrics(
+        event_label_list=reference_event_list.unique_event_labels,
+        time_resolution=1.0
+    )
+
+    for file in reference_event_list.unique_files:
+        reference_event_list_for_current_file = reference_event_list.filter(file=file)
+        estimated_event_list_for_current_file = estimated_event_list.filter(file=file)
+        segment_based_metrics.evaluate(
+            reference_event_list=reference_event_list_for_current_file,
+            estimated_event_list=estimated_event_list_for_current_file
+        )
+    results = segment_based_metrics.results()
+    nose.tools.assert_almost_equals(results['overall']['accuracy']['accuracy'], 0.5)
+    nose.tools.assert_almost_equals(results['overall']['error_rate']['error_rate'], 0.555555555555)
+
+
+def test_direct_use_event():
+    reference_event_list = sed_eval.util.EventList(
+        [
+            {
+                'event_label': 'car',
+                'event_onset': 0.0,
+                'event_offset': 2.5,
+                'file': 'audio/street/b099.wav',
+                'scene_label': 'street'
+            },
+            {
+                'event_label': 'car',
+                'event_onset': 2.8,
+                'event_offset': 4.5,
+                'file': 'audio/street/b099.wav',
+                'scene_label': 'street'
+            },
+            {
+                'event_label': 'car',
+                'event_onset': 6.0,
+                'event_offset': 10.0,
+                'file': 'audio/street/b099.wav',
+                'scene_label': 'street'
+            }
+        ]
+    )
+
+    estimated_event_list = sed_eval.util.EventList(
+        [
+            {
+                'event_label': 'car',
+                'event_onset': 0.2,
+                'event_offset': 3.5,
+                'file': 'audio/street/b099.wav',
+                'scene_label': 'street'
+            },
+            {
+                'event_label': 'car',
+                'event_onset': 6.0,
+                'event_offset': 8.0,
+                'file': 'audio/street/b099.wav',
+                'scene_label': 'street'
+            }
+        ]
+    )
+    event_based_metrics = sed_eval.sound_event.EventBasedMetrics(
+        event_label_list=reference_event_list.unique_event_labels,
+        t_collar=0.20
+    )
+
+    for file in reference_event_list.unique_files:
+        reference_event_list_for_current_file = reference_event_list.filter(file=file)
+        estimated_event_list_for_current_file = estimated_event_list.filter(file=file)
+        event_based_metrics.evaluate(
+            reference_event_list=reference_event_list_for_current_file,
+            estimated_event_list=estimated_event_list_for_current_file
+        )
+    results = event_based_metrics.results()
+    nose.tools.assert_almost_equals(results['overall']['f_measure']['f_measure'], 0.8)
+    nose.tools.assert_almost_equals(results['overall']['error_rate']['error_rate'], 0.3333333333333)
